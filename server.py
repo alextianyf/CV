1248d026dc525e4c173e7a18b40cbba97c0e0d98
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-07-08 16:21:22 -0700

    footer icon path update

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year = current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
