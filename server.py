3ea9daf7598f5c403202500c28f6f8118397a479
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-07-08 22:14:08 -0700

    Flask Rending Completes

--- server.py
+++ server.py
@@ -8,5 +8,13 @@ def home():
     current_year = datetime.datetime.now().year
     return render_template("index.html", year = current_year)
 
+@app.route("/aboutme")
+def get_aboutme():
+    return render_template("aboutme.html")
+
+@app.route("/projects")
+def get_projects():
+    return render_template("projects.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
