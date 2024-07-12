5defa20b999fe76b4d1ee62d6ea2010b45925557
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-07-12 16:27:07 -0700

    V2.2 Flask Framework

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import logging
 app = Flask(__name__)
 freezer = Freezer(app)
 
+
 @app.route('/index.html')
 def get_home():
     return render_template('index.html')
@@ -19,11 +20,13 @@ def get_projects():
 def get_contact():
     return render_template('contact_page.html')
 
+
+
 if __name__ == '__main__':
     # Freeze the application
     freezer.freeze()
     
-    # Define source and destination paths
+    # Copy index.html from build folder into root folder
     source_path = os.path.join('build', 'index.html')
     destination_path = os.path.join('index.html')
     
@@ -35,3 +38,31 @@ if __name__ == '__main__':
         logging.error(f'The file {source_path} does not exist')
     except Exception as e:
         logging.error(f'An error occurred while copying the file: {e}')
+
+
+    # Copy contact_page.html from build folder into public folder
+    source_path = os.path.join('build', 'contact_page.html')
+    destination_path = os.path.join('contact_page.html')
+    
+    try:
+        # Copy index.html from build folder to root folder
+        shutil.copyfile(source_path, destination_path)
+        logging.info(f'Copied {source_path} to {destination_path}')
+    except FileNotFoundError:
+        logging.error(f'The file {source_path} does not exist')
+    except Exception as e:
+        logging.error(f'An error occurred while copying the file: {e}')
+
+    # Copy contact_page.html from build folder into public folder
+    source_path = os.path.join('build', 'projects_page.html')
+    destination_path = os.path.join('projects_page.html')
+    
+    try:
+        # Copy index.html from build folder to root folder
+        shutil.copyfile(source_path, destination_path)
+        logging.info(f'Copied {source_path} to {destination_path}')
+    except FileNotFoundError:
+        logging.error(f'The file {source_path} does not exist')
+    except Exception as e:
+        logging.error(f'An error occurred while copying the file: {e}')
+
