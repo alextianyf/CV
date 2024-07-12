70ccedc71f4d3381eacecd38fe37be94a97110fd
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-07-12 15:43:47 -0700

    V2.1 script auto copy index.html file

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
+import shutil
+import os
+import logging
 
 app = Flask(__name__)
 freezer = Freezer(app)
@@ -17,4 +20,18 @@ def get_contact():
     return render_template('contact_page.html')
 
 if __name__ == '__main__':
+    # Freeze the application
     freezer.freeze()
+    
+    # Define source and destination paths
+    source_path = os.path.join('build', 'index.html')
+    destination_path = os.path.join('index.html')
+    
+    try:
+        # Copy index.html from build folder to root folder
+        shutil.copyfile(source_path, destination_path)
+        logging.info(f'Copied {source_path} to {destination_path}')
+    except FileNotFoundError:
+        logging.error(f'The file {source_path} does not exist')
+    except Exception as e:
+        logging.error(f'An error occurred while copying the file: {e}')
